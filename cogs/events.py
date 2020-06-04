4fef424999e84485467b8ef53282719e998af3b6
BManRL <BManRL@hotmail.com>
2020-06-04 10:26:02 -0300

    Population/Stats handling

--- cogs/events.py
+++ cogs/events.py
@@ -8,5 +8,11 @@ class Events(commands.Cog):
     async def on_member_join(self, member):
         await dbupdate('data.db', 'INSERT INTO players (ID, Name, MMR, Team, Logo) VALUES (?, ?, ?, ?, ?)', (member.id, f"{member.name}#{member.discriminator}", None, None, member.avatar_url,))
 
+        await dbupdate('data.db', 'UPDATE stats SET Members=?', (member.guild.member_count,))
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        await dbupdate('data.db', 'UPDATE stats SET Members=?', (member.guild.member_count,))
+
 def setup(bot):
     bot.add_cog(Events(bot))
