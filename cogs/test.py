0a328d9e63f4754d70759906d2509689fd43dd67
BManRL <BManRL@hotmail.com>
2020-06-04 10:21:13 -0300

    cogs.test initialization

--- cogs/test.py
+++ cogs/test.py
@@ -7,5 +7,9 @@ class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command(name='test')
+    async def _test(self, ctx):
+        pass
+
 def setup(bot):
     bot.add_cog(Test(bot))
