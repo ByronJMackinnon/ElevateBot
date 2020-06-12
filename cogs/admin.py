29e9fb7dacdffa28074a85c389dd57a1e72d6b10
BManRL <BManRL@hotmail.com>
2020-06-12 10:39:05 -0300

    Added members to searchable objects

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import typing
 
 import discord
@@ -74,6 +75,64 @@ class Admin(commands.Cog):
 
         await ctx.send(embed=embed)
 
+    @_search.command(name="member")
+    async def _search_member(self, ctx, member: discord.Member = None):
+        if member is None:
+            member = ctx.author
+
+        roles = [role.mention for role in member.roles]
+        clean_roles = roles[1:]
+        if clean_roles == []:
+            clean_roles = ["None"]
+
+        activity_list = [activity.name for activity in member.activities]
+        if activity_list == []:
+            activity_list = ["None"]
+        
+        embed = discord.Embed(color=member.top_role.color, description=f"Member information for {member.mention}\nID: {member.id}")
+        embed.add_field(name="Status:", value=member.status)
+        embed.add_field(name="Bot:", value=member.bot)
+        embed.add_field(name="Activity:", value='\n'.join(activity_list), inline=False)
+        embed.set_thumbnail(url=member.avatar_url)
+        embed.add_field(name="Created At:", value=getDuration(member.created_at), inline=False)
+        embed.add_field(name="Joined At:", value=getDuration(member.joined_at), inline=False)
+        embed.add_field(name="Roles:", value=', '.join(clean_roles), inline=False)
+
+        await ctx.send(embed=embed)
+
+def clean_time(dtobject):
+    return dtobject.strftime("%B %d, %Y - %I:%M%p")
+
+def getDuration(then, now = datetime.now(), interval = 'default'):
+
+    # Returns a duration as specified by variable interval
+    # Functions, except totalDuration, returns [quotient, remainder]
+
+    duration = now - then # For build-in functions
+    duration_in_s = duration.total_seconds() 
+
+    def years():
+      return divmod(duration_in_s, 31536000) # Seconds in a year=31536000.
+
+    def days(seconds = None):
+      return divmod(seconds if seconds != None else duration_in_s, 86400) # Seconds in a day = 86400
+
+    def hours(seconds = None):
+      return divmod(seconds if seconds != None else duration_in_s, 3600) # Seconds in an hour = 3600
+
+    def totalDuration():
+        y = years()
+        d = days(y[1]) # Use remainder to calculate next variable
+        h = hours(d[1])
+
+        return "{} years, {} days, {} hours".format(int(y[0]), int(d[0]), int(h[0]))
+    
+    return {
+        'years': int(years()[0]),
+        'days': int(days()[0]),
+        'hours': int(hours()[0]),
+        'default': totalDuration()
+    }[interval]
 
 
 def setup(bot):
